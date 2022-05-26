2cfd8d21255658b2c2bdb9337c290931d980ff22
mercyarrobov <myarrobo@espe.edu.ec>
2022-05-26 01:21:11 -0500

    Importación de flask y creación de los 3 controladores

---
+++ todoapp.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+#Instanciar la aplicacion
+app = Flask(__name__)
+
+#Decorador para definir la ruta
+@app.route('/')
+#Función mostrar lista actual de las tareas pendiente
+def mostrar_lista_actual():
+    return 'contro1'
+
+#Decorador para definir la ruta
+@app.route('/enviar')
+#Función aceptar datos y agregar elementos de la lista de tareas pendientes
+def agregar_elemento():
+    return 'contro2'
+
+#Decorador para definir la ruta
+@app.route('/borrar')
+#Función mostrar lista actual de las tareas pendiente
+def borrar_lista():
+    return 'contro3'
+
+
+#main del programa
+if __name__ == "__main__":
+    app.run(debug=True)
