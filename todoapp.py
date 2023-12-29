cbcaf04e0c0bfc7e87899082484b6e089e133af6
Mercy Arrobo <75482232+mercyarrobov@users.noreply.github.com>
2023-12-29 13:04:37 -0500

    Versión Final

--- todoapp.py
+++ todoapp.py
@@ -24,17 +24,24 @@ def mostrar_lista_actual():
     return render_template('base.html', Lista_t=Lista_t)
 
 #Decorador para definir la ruta
-@app.route('/enviar')
+@app.route('/enviar', methods=['POST'])
 #Función aceptar datos y agregar elementos de la lista de tareas pendientes
 def agregar_elemento():
-    return 'contro2'
+    if request.method == 'POST':
+        nuevo_titulo = request.form['nuevo_titulo']
+        nueva_tarea = modelo(titulo=nuevo_titulo, marca_complet=False)
+        db.session.add(nueva_tarea)
+        db.session.commit()
+    return redirect(url_for('mostrar_lista_actual'))
 
 #Decorador para definir la ruta
-@app.route('/borrar')
+@app.route('/borrar/<int:id_tarea>')
 #Función mostrar lista actual de las tareas pendiente
-def borrar_lista():
-    return 'contro3'
-
+def borrar_lista(id_tarea):
+    tarea_a_borrar = modelo.query.get(id_tarea)
+    db.session.delete(tarea_a_borrar)
+    db.session.commit()
+    return redirect(url_for('mostrar_lista_actual'))
 
 #main del programa
 if __name__ == "__main__":
